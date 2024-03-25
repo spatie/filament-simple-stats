bda9066a61ae9ff85e278c4e8c75a89f477d51f5
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-03-25 13:56:30 +0100

    Cleanup tests.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,26 +11,5 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Spatie\\FilamentSimpleStat\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            FilamentSimpleStatServiceProvider::class,
-        ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_filament-simple-stats_table.php.stub';
-        $migration->up();
-        */
     }
 }
